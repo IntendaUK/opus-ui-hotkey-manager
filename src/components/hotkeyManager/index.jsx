7023cef3a168fca368c96b2adb773757d9f0d58c
Shaun <skichenbrand@gmail.com>
2024-09-17 17:23:18 +0200

    bug: clearing keys when page receives focus (after having been blurred) so we can clear control when ctrl+tab is pressed

--- src/components/hotkeyManager/index.jsx
+++ src/components/hotkeyManager/index.jsx
@@ -1,15 +1,29 @@
 //System
-import { createContext } from '@intenda/opus-ui';
+import { useEffect } from 'react';
+import { createContext, } from '@intenda/opus-ui';
 
 //Components
 import KeyEventListener from './components/KeyEventListener';
 
+//Helpers
+import { clearKeysBuffer } from './manager';
+
 //Context
 const HotkeyManagerContext = createContext('hotkeyManager');
+
+//Events
+const onMount = () => {
+	document.addEventListener('visibilitychange', () => {
+		if (!document.hidden)
+			clearKeysBuffer();
+	});
+};
 
 //Exports
 export const HotkeyManager = props => {
 	const { id, classNames, style, attributes } = props;
+
+	useEffect(onMount, []);
 
 	return (
 		<HotkeyManagerContext.Provider value={props}>
